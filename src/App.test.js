8363254ec857b6a9dfe0950fb77983aca4d11ec7
RicholeYu <richole.yu@qq.com>
2022-08-12 16:25:38 +0800

    更新样式

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("App", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/周平均心情指数/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("App number", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/五/i);
   expect(linkElement).toBeInTheDocument();
 });
